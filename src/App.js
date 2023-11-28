49ace59d74e6df17368feead15d2341281b60778
3cia01 <monyepatricia1@gmail.com>
2023-11-28 11:00:39 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/3cia01/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {""}
+          open sourced and coded by Patricia Monye
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
