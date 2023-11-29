0d699205a70e8be8f5c87c41d0f311cddfbfc450
3cia01 <monyepatricia1@gmail.com>
2023-11-29 11:47:00 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <a
-          href="https://github.com/3cia01/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {""}
-          open sourced and coded by Patricia Monye
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Patricia Monye,
+          <a
+            href="https://github.com/3cia01/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {""}
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
